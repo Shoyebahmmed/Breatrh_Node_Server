50bdb8fec4d5d53db3ae3fb95b26d2952d6f9ed5
Shoyebahmmed <ahmmad.ef22@gmail.com>
2023-05-10 12:38:11 +1000

    port

--- Server/server_conn.js
+++ Server/server_conn.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 8888;
 
 
-app.listen(8888, () => {
+app.listen(port, () => {
   console.log('Server listening on port 8888');
 });
 
